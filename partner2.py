2846beeaf839937473decb5335e4cb95b10fdc11
kiari <pillsthepill300@gmail.com>
2026-02-16 12:09:11 -0600

    Co-authored-by: EmiYong <EmiYong@users.noreply.github.com>

---
+++ partner2.py
@@ -0,0 +1,19 @@
+print("I am partner 2")
+# Partner 1: Emiliano Gutierrez 
+# Partner 2: Luis Cisneros
+#-------------------------------
+
+"""
+lines 
+of
+comments
+"""
+#asterick wall
+#******************************
+# This line of code does nothing 
+
+# This line of code wants to do something
+
+# This line of code thinks line 15 is stupid
+
+print ("hello world" )
